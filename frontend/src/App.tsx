f248315dae0bedb189be38ed32ab28aaeadb79a1
kyohei328 <zwei328@gmail.com>
2023-10-27 05:46:24 +0900

    add_footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 
 import './App.css'
-import { Link } from "react-router-dom"
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { css } from '@emotion/react'
+
+const Styles = ({
+  heightstyle: css ({
+    height: '69vh',
+  })
+})
+
 
 function App(): JSX.Element {
 
   return (
-     <MantineProvider>
+    <MantineProvider>
       <Header/>
         <div>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <h1  css={Styles.heightstyle} className="text-3xl font-bold underline">Hello world!</h1>
         </div>
-      </MantineProvider>
+      <Footer />
+    </MantineProvider>
   )
 }
 
